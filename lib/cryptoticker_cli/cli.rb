714b2422288cea57b3c08e6acead962ca471005e
Asim Ali <asimsali95@gmail.com>
2021-08-26 01:41:23 -0500

    project complete, final commit

--- lib/cryptoticker_cli/cli.rb
+++ lib/cryptoticker_cli/cli.rb
@@ -9,13 +9,17 @@ class Cryptoticker::CLI   #name needs to be changed
         end
         #binding.pry
         menu
-        
     end
 
     def menu
         puts ""
-        puts "Enter the name or number of the cryptocurrency you would like to view:"
-        input = gets.strip
+        puts "Instructions:"
+        puts "1. Enter the name or number of the cryptocurrency you would like to view."
+        puts "2. Or enter 'list' to view the full list of cryptocurrencies at any time."
+        puts "3. Lastly enter 'exit' to end the program."
+        puts ""
+        puts "Enter your command:"
+        input = gets.chomp
 
         if input.to_i > 0 && input.to_i <= 24
             crypto = Crypto.all[input.to_i - 1]
@@ -23,11 +27,18 @@ class Cryptoticker::CLI   #name needs to be changed
             puts "Ticker: #{crypto.ticker}"
             puts "Full name: #{crypto.full_name}"
             puts "Price: $#{crypto.current_price}"
-            puts "% Change: #{crypto.percent_change}"
-          menu
-        elsif input == "exit" || "quit"
+            puts "24 HR % Change: #{crypto.percent_change}"
+            menu
+        elsif input == "list"
+            Crypto.all.each.with_index(1) do |app, id|
+                puts "#{id}. #{app.ticker}"
+            end
+            menu
+        elsif input == "exit"
             puts ""
             puts "Thank you for using CryptoTicker CLI. Goodbye!"
+            puts ""
+            exit
         else
             puts ""
             puts "Invalid choice, please enter a number between 1-24:"
